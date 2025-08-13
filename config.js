e955044cf6ea4d32ae1477fc7b85ecd6f2e21444
Hussien Kamal <hussien.kamal92@gmail.com>
2025-08-13 03:51:18 +0300

    Remove unused image configurations for sunbula, yallahikes, and fireflylife from config.js

--- config.js
+++ config.js
@@ -7,16 +7,6 @@ const emailjsConfig = {
 
 // Image configurations
 const imageConfigs = {
-  sunbula: {
-    src: 'images/entities/sunbula.jpg',
-    url: 'https://www.instagram.com/sunbulapan/',
-    blocks: [0, 1, 2, 3, 100, 101, 102, 103],
-  },
-  yallahikes: {
-    src: 'images/entities/yallahikes.jpg',
-    url: 'https://www.instagram.com/yallahikes/',
-    blocks: [1316, 1317, 1416, 1417],
-  },
   datirs: {
     src: 'images/entities/datirs.jpg',
     url: 'https://www.datirs.com/',
@@ -24,10 +14,5 @@ const imageConfigs = {
       4152, 4153, 4154, 4155, 4156, 4157, 4158, 4252, 4253, 4254, 4255, 4256,
       4257, 4258, 4352, 4353, 4354, 4356, 4357, 4358,
     ],
-  },
-  fireflylife: {
-    src: 'images/entities/fireflylife.jpg',
-    url: 'https://www.fireflylife.org/',
-    blocks: [6330, 6331, 6332, 6430, 6431, 6432],
   },
 };
